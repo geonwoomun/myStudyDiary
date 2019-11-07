eb6765b73b6ad18e5315d237d2faccf120a99fd6
geonwoomun <ansejrrhkd@naver.com>
2019-11-07 21:49:53 +0900

    apikey 때문에 삭제

--- JavaScript/웹스터디 관련../weather.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-let APIKEY = 'e73b469f2c515631b3ddcf5dbf110a3d';
-
-axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London&APPID=${APIKEY}`)
-.then(results => {
-    console.log(results.data);
-})
