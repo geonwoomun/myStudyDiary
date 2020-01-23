f319cfec771a014d4deecfe33ba091b1d1875c8c
geonwoomun <ansejrrhkd@naver.com>
2020-01-23 14:50:09 +0900

    타입스크립트 공부!! 해보기!

---
+++ typeScript/dist/practice.js
@@ -0,0 +1,6 @@
+"use strict";
+function sumArray(numbers) {
+    return numbers.reduce(function (acc, current) { return acc + current; }, 0);
+}
+var total = sumArray([1, 2, 3, 4, 5]);
+console.log(total);
