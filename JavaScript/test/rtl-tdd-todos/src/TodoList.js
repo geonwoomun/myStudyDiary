b7952cf1c263dbc47507281d3dc45e876e6847bd
geonwoomun <ansejrrhkd@naver.com>
2020-02-10 21:56:25 +0900

    TDD 기반 todolist 만들어보기

---
+++ JavaScript/test/rtl-tdd-todos/src/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = ({todos, onToggle, onRemove}) => {
+    return (
+        <ul>
+            {todos.map(todo => (
+                <TodoItem todo={todo} key={todo.id}
+                onToggle={onToggle}
+                onRemove={onRemove}
+                />
+            ))}
+        </ul>
+    );
+};
+
+export default TodoList;
